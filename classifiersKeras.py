397cf41d0160a5704f7c09dbb49892e282d23de4
Brieuc <pinon.brieuc@gmail.com>
2017-12-11 11:40:44 +0100

    rerien

--- classifiersKeras.py
+++ classifiersKeras.py
@@ -17,7 +17,7 @@ To run this file you will need :
     -keras
     For model visualization (optionnal just comment "plot_model()" call if you do not need it:
     -pydot
-    -graphiz (apt-get graphviz, not the anaconda package)
+    -graphviz (apt-get graphviz, not the anaconda package)
 
 @author: brieuc
 """
