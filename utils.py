f2b260ddb4fdc76477056d153fdac353bda4dbf1
hdevillez <henri.devillez@gmail.com>
2017-12-11 11:26:59 +0100

    Start final notebook

---
+++ utils.py
@@ -0,0 +1,13 @@
+import csv
+
+def read_files(filename):
+    training_list = []
+    label_list = []
+    file = open(filename, "r")
+    reader = csv.reader(file, delimiter=';')
+    for tweet, author in reader:
+        training_list.append(tweet)
+        label_list.append(author)
+    file.close()
+    
+    return {'x' : training_list, 'label' : label_list}
